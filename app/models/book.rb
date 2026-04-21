600adb26b1e6a0b83f20988ccd1f9f065970e292
hamzaiftikhhar <alihamxa366@gmail.com>
2026-04-21 12:36:41 +0500

    This is bug

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,12 +9,12 @@ class Book < ApplicationRecord
   scope :costs_more_than, ->(amount) { where("price > ?", amount) }
 
 
-  
+   #this is the instance method that returns the highlighted reviews for a book. It checks if the number of reviews for the book is greater than or equal to 2. If it is, it returns all the reviews; otherwise, it returns an empty ActiveRecord relation (Review.none). This method can be used to display highlighted reviews for a book on a webpage or in an application.
   def highlighted_reviews
   if reviews.count >= 2
     reviews
   else
-    Review.none
+    Review.none #this is the bug 
   end
   end
 end
