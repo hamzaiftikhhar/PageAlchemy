c6357f47aa2d9eb0ad6ae2111b233010cb17e284
hamzaiftikhhar <alihamxa366@gmail.com>
2026-04-21 11:46:43 +0500

    this the stagging comment for book

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,8 +5,9 @@ class Book < ApplicationRecord
   has_many :reviews
   has_and_belongs_to_many :orders
 
-  scope :in_print, -> { where(out_of_print: false) }
+  scope :in_print, -> { where(out_of_print: false) } #This is the scope function
   scope :costs_more_than, ->(amount) { where("price > ?", amount) }
+
 
   
   def highlighted_reviews
@@ -17,5 +18,3 @@ class Book < ApplicationRecord
   end
   end
 end
-
-
